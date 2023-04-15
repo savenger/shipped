e537ff5ebc77b54dcdbc14e9d2539b219e23fd10
Sascha Mahmood <github@smahmood.de>
2023-04-15 21:33:00 +0200

    added some gamepad buttons

--- level/CameraToggle.gd
+++ level/CameraToggle.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 	_cameras = CameraManager.get_cameras()
 
 func _unhandled_input(event):
-	if event is InputEventKey and event.keycode == KEY_C and event.pressed:
+	if (event is InputEventJoypadButton and event.button_index == JOY_BUTTON_RIGHT_SHOULDER and event.pressed) or (event is InputEventKey and event.keycode == KEY_C and event.pressed):
 		if _index + 1 >= len(_cameras):
 			_index = 0
 		else:	
