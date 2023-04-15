87009f4dd16951fa9b49204080850a99bb984d8b
Sascha Mahmood <github@smahmood.de>
2023-04-15 12:04:08 +0200

    added static path3d river

--- level/terrain.gd
+++ level/terrain.gd
@@ -7,7 +7,7 @@ var chunkSize : int = 16
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_generateCSGGround()
+	#_generateCSGGround()
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
