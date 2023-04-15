bf45a64c180e45286e35a1f6ab551a91779f68a2
Filip Lundby <filip@filiplundby.dk>
2023-04-15 19:06:04 +0200

    Lightning

---
+++ ThunderStorm/ThunderStorm.gd
@@ -0,0 +1,19 @@
+extends Area3D
+
+
+var _delay: float = 3.0
+var _timer: float = 0
+
+
+func _process(delta):
+	if !$LightningSprite.visible:
+		_timer += delta
+	
+	if _timer > _delay:
+		_timer = 0
+		_delay = randf_range(1, 5)
+		var frame_index = randi_range(0, $LightningSprite.hframes - 1)
+		$LightningSprite.frame = frame_index
+		$LightningSprite.visible = true
+		await get_tree().create_timer(.2).timeout
+		$LightningSprite.visible = false
