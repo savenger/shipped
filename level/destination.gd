1b40170d631cd05c86d0bc61db38f5cf0d2aae56
Filip Lundby <filip@filiplundby.dk>
2023-04-16 13:35:37 +0200

    Chagne camera angle at destination

--- level/destination.gd
+++ level/destination.gd
@@ -6,6 +6,9 @@ signal unloading(amount: float)
 @export var _expected_delivery: float = 100
 @export var _unload_speed: float = 0.08
 
+@export var destination_camera: CameraViewpoint
+@export var ship_camera: CameraViewpoint
+
 var _unloaded_amount: float = 0
 var _is_unloading: bool = false
 var _timer = 0
@@ -22,6 +25,7 @@ func _process(delta):
 		_is_unloading = false
 		_unloaded_amount = 0
 		print("destination: delivered")
+		CameraManager.set_current_camera(ship_camera)
 		return
 		
 	_timer += delta
@@ -41,5 +45,5 @@ func _on_area_3d_body_entered(body):
 		return
 	if body.cargo_amount < _expected_delivery:
 		return
-	
+	CameraManager.set_current_camera(destination_camera)
 	_is_unloading = true
